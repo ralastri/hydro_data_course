bcc1c1b794d991e03ccb3d7663c134553ff9af22
ralastri <raffaello.lastrico@yahoo.de>
2022-01-11 13:52:36 +0100

    Projekt für Übungen 1-4 erstellt

---
+++ exercise_01_data_structure.R
@@ -0,0 +1,60 @@
+## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ##
+# 
+# HEADER FÜR R CODE  -- Überschrift und Kurze Beschreibung hier einfügen!
+#
+#
+# Raffaello Lastrico, DATUM
+#
+## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ### ## ##
+
+
+# Setup  ------------------------------------------------------------------
+
+# alte R-Objekte loeschen
+rm(list=ls())
+
+# Hauptpfad Arbeitsverzeichnis (automatisch oder händisch einfügen)
+hauptPfad <- dirname(rstudioapi::getActiveDocumentContext()$path)
+#hauptPfad <- "C:/Users/Dell/Documents/02 UNI Master Hydro/1. Semester/01 HYDPRO/Einführung in R - Klemens Rosin/"
+
+# Arbeitsverzeichnis setzen (falls nicht gewünscht auskommentieren)
+setwd(hauptPfad)
+
+
+
+# benoetigte Pakete laden -------------------------------------------------
+
+
+# Funktionen definieren ---------------------------------------------------
+
+
+
+# Datenimport -------------------------------------------------------------
+# z.B.:
+importPfad = paste0(hauptPfad, "/1_Daten/speedtest_tageswerte.csv")
+internet <- read.csv(importPfad)
+
+
+
+
+
+# Level 1 -----------------------------------------------------------------
+
+## Erste Sub-Section ------------------------------------------------------
+
+### Erste sub sub section -------------------------------------------------
+
+### Zeite sub sub ----
+
+#### Erste sub sub sub section ----
+
+
+
+# Zweite Section ------------------------------------------------------------
+
+
+
+
+
+
+# Why ====
